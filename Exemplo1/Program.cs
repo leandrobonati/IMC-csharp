e6e2fe7cfb64242d57e85182ca135ed80733f5bb
Leandro Bonati <161650868+leandrobonati@users.noreply.github.com>
2025-02-17 11:51:03 -0300

    Add files via upload

---
+++ Exemplo1/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Cálculo IMC");
+Console.WriteLine("Digite seu Nome:");
+string nome = Console.ReadLine();
+Console.WriteLine("Digite seu Peso:");
+double peso =Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Digite seu Altura:");
+double altura = Convert.ToDouble(Console.ReadLine());
+
+double imc = peso / Math.Pow(altura,2);
+
+Console.WriteLine($"{nome} o seu IMC é de {imc}");
+
+if  (imc > 30)
+{
+    Console.WriteLine("Você é Obesoooooooo");
+}
+else if (imc < 18.5)
+{
+    Console.WriteLine("Baixo peso");
+}
+else if (imc >= 18.5 &&  imc <= 24.9)
+{
+    Console.WriteLine("Peso normal");
+}
+else if (imc >=25 && imc <=29.9)
+{
+    Console.WriteLine("excesso de peso");
+}
+
+Console.ReadKey();
